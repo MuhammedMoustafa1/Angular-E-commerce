98db9f9a0eacabe930d56a88daa4a07fa9dda4cc
mohamed mostafa <muhammed.moustafa.ahmed@gmail.com>
2024-05-08 00:53:15 +0300

    component lifecycle

--- src/app/Directives/light-box.directive.ts
+++ src/app/Directives/light-box.directive.ts
@@ -1,15 +1,18 @@
-import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Directive({
   selector: '[LightBox]'
 })
-export class LightBoxDirective {
+export class LightBoxDirective implements OnChanges{
   //private elemRef;
   @Input('LightBox') highLightColor:string="yellow";
   @Input() defaultColor:string="darkblue";
 
   constructor(private elemRef: ElementRef) {
-    elemRef.nativeElement.style.border="2px solid darkblue";
+    // elemRef.nativeElement.style.border="2px solid darkblue";
+  }
+  ngOnChanges(): void {
+    this.elemRef.nativeElement.style.border=`2px solid ${this.defaultColor}` ;
   }
   @HostListener('mouseover') onMouseOver(){
     this.elemRef.nativeElement.style.border=`3px solid ${this.highLightColor}`;
