b7af938083892215c47ec6f97dee3405f901bfe5
mohamed mostafa <muhammed.moustafa.ahmed@gmail.com>
2024-05-10 23:29:34 +0300

    @view child

--- src/app/Components/Order/order-master/order-master.component.ts
+++ src/app/Components/Order/order-master/order-master.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ICategory } from 'src/app/Models/icategory';
+import { ProductlistComponent } from '../productlist/productlist.component';
 
 @Component({
   selector: 'app-order-master',
   templateUrl: './order-master.component.html',
   styleUrls: ['./order-master.component.scss']
 })
-export class OrderMasterComponent implements OnInit {
+export class OrderMasterComponent implements OnInit,AfterViewInit {
   catList: ICategory[];
   selectedCatID: number = 0;
   recivedOrderTotalPrice:number=0;
+  //clientNameInpElem:ElementRef = new ElementRef();
+  //clientNameInpElem:ElementRef | undefined = undefined;
+  //clientNameInpElem:ElementRef | null = null;
+  //clientNameInpElem?:ElementRef={} as ElementRef; //optional;
+  @ViewChild('clientNameInp') clientNameInpElem!:ElementRef;      //Non-null asseration operator
+  @ViewChild(ProductlistComponent) prdListCompObj!: ProductlistComponent;
   constructor(){
     this.catList = [
       {
@@ -29,7 +36,19 @@ export class OrderMasterComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(): void {
+    this.clientNameInpElem.nativeElement.value = "Your Nmae here";
+    this.clientNameInpElem.nativeElement.style.border="2px solid red";
+    console.log(this.prdListCompObj.prdList);
+  }
+
   onTotalPriceChanged(totalPrice: number){
     this.recivedOrderTotalPrice = totalPrice;
   }
+  completeOreder(){
+    // for test
+    this.prdListCompObj.prdList[0].quantity-=1;
+
+  }
+
 }
